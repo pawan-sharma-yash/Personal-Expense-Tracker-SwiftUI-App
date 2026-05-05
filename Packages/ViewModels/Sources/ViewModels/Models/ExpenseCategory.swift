baca9c2c2872d28854703c012998e5d8ee9a3f05
Pawan Sharma <sharma.pawan@yash.com>
2026-05-05 17:52:26 +0530

    Minor code refactoring

--- Packages/ViewModels/Sources/ViewModels/Models/ExpenseCategory.swift
+++ Packages/ViewModels/Sources/ViewModels/Models/ExpenseCategory.swift
@@ -1,26 +1,14 @@
 public enum ExpenseCategory: String, CaseIterable, Identifiable, Sendable {
-	case food
-	case transport
-	case shopping
-	case home
-	case fun
-	case health
-	case education
-	case other
+	case food = "Food"
+	case transport = "Transport"
+	case shopping = "Shopping"
+	case home = "Home"
+	case fun = "Fun"
+	case health = "Health"
+	case education = "Education"
+	case other = "Other"
 
-	public var id: String { rawValue }
-
-	public var title: String {
-		switch self {
-		case .food: "Food"
-		case .transport: "Transport"
-		case .shopping: "Shopping"
-		case .home: "Home"
-		case .fun: "Fun"
-		case .health: "Health"
-		case .education: "Education"
-		case .other: "Other"
-		}
-	}
+	public var id: String { self.rawValue }
+	public var title: String { self.rawValue }
 }
 
