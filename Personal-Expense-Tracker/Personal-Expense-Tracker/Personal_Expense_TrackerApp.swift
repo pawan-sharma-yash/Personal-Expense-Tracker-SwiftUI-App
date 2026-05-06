79eb6b005b828046c3e97f75c6f9bf2b12628d41
Pawan Sharma <sharma.pawan@yash.com>
2026-05-06 16:08:31 +0530

    [AUTO-COMMIT] Generated commit message

--- Personal-Expense-Tracker/Personal-Expense-Tracker/Personal_Expense_TrackerApp.swift
+++ Personal-Expense-Tracker/Personal-Expense-Tracker/Personal_Expense_TrackerApp.swift
@@ -7,30 +7,7 @@
 
 import SwiftUI
 import UIKit
-import Observation
 import DesignSystem
-
-fileprivate extension UISegmentedControl {
-	static func _customAppearance() {
-		// Customize the segmented control appearance
-		UISegmentedControl.appearance().backgroundColor = UIColor(DS.ColorToken.surface)
-		UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(DS.ColorToken.accent)
-
-		// Set font for normal state
-		UISegmentedControl.appearance()
-			.setTitleTextAttributes([
-				.foregroundColor: UIColor.darkGray,
-				.font: UIFont.systemFont(ofSize: 15, weight: .semibold)
-			], for: .normal)
-
-		// Set font for selected state
-		UISegmentedControl.appearance()
-			.setTitleTextAttributes([
-				.foregroundColor: UIColor.white,
-				.font: UIFont.systemFont(ofSize: 15, weight: .bold)
-			], for: .selected)
-	}
-}
 
 @main
 struct Personal_Expense_TrackerApp: App {
