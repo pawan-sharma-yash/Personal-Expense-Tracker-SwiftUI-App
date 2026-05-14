ff0be5bb728f234bb4b166d062d9ac66ec17c296
Pawan Sharma <sharma.pawan@yash.com>
2026-05-14 18:08:40 +0530

    [WIP] - More granular control over radius

--- Packages/DesignSystem/Sources/DesignSystem/Metrics/Radius.swift
+++ Packages/DesignSystem/Sources/DesignSystem/Metrics/Radius.swift
@@ -10,8 +10,12 @@ import SwiftUI
 
 extension DS.Metrics {
 	public enum Radius {
+		public static let xxs : CGFloat = 6
+		public static let xs : CGFloat = 8
 		public static let s: CGFloat = 10
-		public static let m: CGFloat = 14
-		public static let l: CGFloat = 18
+		public static let m: CGFloat = 12
+		public static let l: CGFloat = 16
+		public static let xl: CGFloat = 20
+		public static let xxl: CGFloat = 24
 	}
 }
