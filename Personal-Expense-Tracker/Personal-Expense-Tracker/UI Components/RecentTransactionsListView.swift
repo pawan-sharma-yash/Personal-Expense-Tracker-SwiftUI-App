139f56b5d87477dff86ab6ba002906e2ad5e2bfa
Pawan Sharma <sharma.pawan@yash.com>
2026-05-20 10:21:10 +0530

    Make recent transations list an independent component
    Move changes to a new file

---
+++ Personal-Expense-Tracker/Personal-Expense-Tracker/UI Components/RecentTransactionsListView.swift
@@ -0,0 +1,58 @@
+//
+//  File.swift
+//  Personal-Expense-Tracker
+//
+//  Created by Pawan Kumar Sharma on 20/05/26.
+//
+
+import SwiftUI
+import ViewModels
+import DesignSystem
+
+struct RecentTransactionsListView: View {
+	let recentTransactions: [ViewModels.Expense]
+	let action: (ViewModels.Expense) -> Void
+
+	var body: some View {
+		List(recentTransactions) { tx in
+			Button(action: { action(tx) }) {
+				ExpenseView(
+					title: tx.title,
+					transactionDate: tx.date.description,
+					amount: tx.amount,
+					category: tx.category
+				)
+			}
+			.listRowSeparator(.hidden)
+			.listRowInsets(listInsets)
+			.padding([.horizontal, .vertical], Spacing.m)
+			.background(
+				RoundedRectangle(cornerRadius: Radius.m)
+					.fill(Color.veryLightBackground)
+					.shadow(color: Color.lightDarkShadow, radius: 1, x: 0, y: 1)
+			)
+			.overlay(
+				RoundedRectangle(cornerRadius: Radius.m)
+					.stroke(Color(.systemGray5), lineWidth: BorderWidth.hairline)
+			)
+		}
+		.listStyle(.plain)
+	}
+}
+
+private extension RecentTransactionsListView {
+	var listInsets: EdgeInsets {
+		EdgeInsets(
+			top: Spacing.xs,
+			leading: 0,
+			bottom: Spacing.xs,
+			trailing: 0
+		)
+	}
+}
+
+private extension RecentTransactionsListView {
+	typealias Spacing = DS.Metrics.Spacing
+	typealias Radius = DS.Metrics.Radius
+	typealias BorderWidth = DS.Metrics.BorderWidth
+}
